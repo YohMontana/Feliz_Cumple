3b7380e0d5f59ee66b9efa0982f1a12821edb951
Yohmontana <estebanjcarlo@gmail.com>
2024-04-23 19:06:41 -0500

    cumpleaños final

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLEAÑOS', ' SUSANA!' ],
+			strings: [ '¡FELIZ', 'CUMPLEAÑOS', ' SUSANA!' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
